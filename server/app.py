0707cb8c1cf86080c8a9369e97c63cdc8580eb8e
faith wangari njau <faithjonnes91@gmail.com>
2025-01-24 10:34:20 +0300

    initial commits

--- server/app.py
+++ server/app.py
@@ -2,4 +2,13 @@
 
 from flask import Flask
 
+
 app = Flask(__name__)
+@app.route('/')
+def user(username):
+ return f'<h1>Profile for {username}</h1>'
+
+if __name__ == '__main__':
+    
+    app.run( debug=True)
+
